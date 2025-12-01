a4cf567d06bfa479f04438ccd1e6e3fd5ac7f476
Milos Zivlak <zivlakmilos@gmail.com>
2025-12-01 16:36:30 +0100

    Day 01 Puzzle 02

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -38,6 +38,37 @@ func solvePuzzle01() {
 }
 
 func solvePuzzle02() {
+	res := 0
+	dial := 50
+
+	input := getInput()
+	for line := range strings.SplitSeq(input, "\n") {
+		rot := parseLine(line)
+
+		isLeft := rot < 0
+		if isLeft {
+			rot = -rot
+		}
+
+		for range rot {
+			if isLeft {
+				dial--
+				if dial == 0 {
+					res++
+				} else if dial < 0 {
+					dial = 99
+				}
+			} else {
+				dial++
+				for dial >= 100 {
+					dial = 0
+					res++
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Password to open door is: %d\n", res)
 }
 
 func main() {
