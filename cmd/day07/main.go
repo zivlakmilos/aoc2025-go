8beeb9bcfd302d149d4a13d36139985d3a5daad8
Milos Zivlak <zivlakmilos@gmail.com>
2025-12-07 18:59:53 +0100

    Day 07 Puzzle 02

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -19,6 +19,29 @@ func parseInput(input string) [][]byte {
 	return res
 }
 
+func solveTimeline(data [][]byte, mem map[uint64]int, row, col int) int {
+	if row >= len(data) {
+		return 1
+	}
+	key := (uint64(row) << 32) | uint64(col)
+	if val, ok := mem[key]; ok {
+		return val
+	}
+
+	res := 0
+
+	if data[row][col] == '^' {
+		res += solveTimeline(data, mem, row+1, col+1)
+		res += solveTimeline(data, mem, row+1, col-1)
+	} else {
+		res += solveTimeline(data, mem, row+1, col)
+	}
+
+	mem[key] = res
+
+	return res
+}
+
 func solvePzzle01() {
 	input := getInput()
 	data := parseInput(input)
@@ -42,6 +65,21 @@ func solvePzzle01() {
 }
 
 func solvePzzle02() {
+	input := getInput()
+	data := parseInput(input)
+
+	col := 0
+	for i := range len(data[0]) {
+		if data[0][i] == 'S' {
+			col = i
+			break
+		}
+	}
+
+	mem := map[uint64]int{}
+	res := solveTimeline(data, mem, 0, col)
+
+	fmt.Printf("Total timelines: %d\n", res)
 }
 
 func main() {
